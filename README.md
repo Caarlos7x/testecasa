073c2f6cd883498e8ab613f27546adb6d4b9fcc2
Carlos Augusto <95699480+Caarlos7x@users.noreply.github.com>
2023-02-28 16:30:30 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# RAZOR SHAVE
+
+<img height="300px" src="https://i.imgur.com/0Thon5y.png">
+
+Página para salvar perfis favoritos do Github.
+
+
+Deploy: [Casa Guntzel](https://www.casaguntzel.com.br)
+
+## 🚀 Começando -
+
+### 📋 Pré-requisitos
+
+Executável em qualquer navegador
+
+### 🔧 Instalação
+
+Não carece de instalação
+
+## 📦 Desenvolvimento
+
+É uma página HTML estilizada em CSS. Programação em JavaScript.
+
+## 🛠️ Construído com
+
+<img align="center" alt="HTML" height="30" width="40" src="https://cdn.worldvectorlogo.com/logos/html-1.svg"><img align="center" alt="CSS" height="30" width="40" src="https://cdn.worldvectorlogo.com/logos/css-3.svg"><img align="center" alt="CSS" height="30" width="40" src="https://cdn.worldvectorlogo.com/logos/javascript-1.svg">
+
+HTML | CSS | JS
+
+## 📌 Versão
+
+Versão única
+
+## ✒️ Autores
+
+- **Carlos Augusto** - _Trabalho Inicial_ - [Carlos Augusto](https://www.linkedin.com/in/carlos-augusto-dantas-frei-51502ba9/)
+
+---
+
+⌨️ com ❤️ por [Carlos Augusto](https://gist.github.com/Caarlos7x) 😊
---
+++ main.js
@@ -0,0 +1,108 @@
+window.addEventListener('scroll', onScroll)
+const navigation = document.querySelector('#navigation')
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function disableScrolling() {
+  setTimeout(function() {
+      document.body.style.overflow = 'hidden';
+  }, 2000);
+}
+
+function enableScrolling() {
+  document.body.style.overflow = '';
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+// number auto counter 
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if(startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+})
+
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
